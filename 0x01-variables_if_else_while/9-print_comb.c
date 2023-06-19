29e15f444388199abd8f3105e365b07604caad16
SamuelWorld20 <ejalonibuoluwaseyi@gmail.com>
2023-06-19 09:05:30 -0700

    Solution to task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - This program prints all possible combinations of single-digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar('0');
+	for (int n = 1; n < 10; n++)
+	{
+		putchar(',');
+		putchar(' ');
+		putchar('0' + n);
+	}
+	return (0);
+}
