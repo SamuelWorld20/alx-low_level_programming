aa07ca5257570127d2a81b4c926762aa669bac02
SamuelWorld20 <ejalonibuoluwaseyi@gmail.com>
2023-06-21 03:50:04 -0700

    Soltuion to task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,23 +10,14 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
 		return (0);
 	}
 	else
 	{
-		_putchar('-');
-		_putchar(',');
-		_putchar(' ');
 		return (-1);
 	}
 	return (0);
