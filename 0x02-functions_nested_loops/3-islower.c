bd29009845abba2fd71623706403553bb606df77
SamuelWorld20 <ejalonibuoluwaseyi@gmail.com>
2023-06-21 02:48:37 -0700

    Solution to task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _islower - This functione checks for a lower case character.
+ *@c: Parameter argument.
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar(return);
+}
