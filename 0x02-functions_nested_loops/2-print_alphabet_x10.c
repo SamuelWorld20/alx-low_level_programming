eb1135288e80ee1b713bc0256fcb6a6e4c9fd1e2
SamuelWorld20 <ejalonibuoluwaseyi@gmail.com>
2023-06-20 18:59:55 -0700

    Solution to task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - This program prints the alphabet 10 time.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int a, b;
+
+	int letter;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 26; b++)
+		{
+			_putchar("%c", letter);
+			letter++;
+		}
+		_putchar("\n");
+		letter = 'a';
+	}
+}
