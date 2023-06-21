43424cd9802118fbe3e84aa40d84bec8d6fa21d0
SamuelWorld20 <ejalonibuoluwaseyi@gmail.com>
2023-06-21 09:58:35 -0700

    Solution to task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - This function adds two integers.
+ * @a: First paramenter argument.
+ * @b: Second parameter argument.
+ *
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
