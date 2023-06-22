080b91affdc4457157605ad5e507b11a1b3819c4
SamuelWorld20 <ejalonibuoluwaseyi@gmail.com>
2023-06-21 18:24:13 -0700

    Solution to task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a;
+
+	unsigned int fib1 = 1;
+
+	unsigned int fib2 = 2;
+
+	unsigned int fibNext;
+
+	printf("%u, %u, ", fib1, fib2);
+	for (int a = 3; a <= 98; a++)
+	{
+		fibNext = fib1 + fib2;
+		printf("%u", fibNext);
+		if (a != 98)
+		{
+			printf(", ");
+		}
+		fib1 = fib2;
+		fib2 = fibNext;
+	}
+	printf("\n");
+	return (0);
+}
