6cd1569dbbc1e2585b9f11042fabd44c26e10d8b
SamuelWorld20 <ejalonibuoluwaseyi@gmail.com>
2023-06-21 17:35:52 -0700

    Solution to task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int fib1 = 1;
+
+	int fib2 = 2;
+
+	int a;
+
+	int nextFib;
+
+	printf("%d, %d", fib1, fib2);
+	for (a = 3; a <= 50; a++)
+	{
+		nextFib = fib1 + fib2;
+		printf("%d", nextFib);
+		if (a != 50)
+		{
+			prinf(", ");
+		}
+		fib1 = fib2;
+		fib2 = nextFib;
+	}
+	printf("\n");
+}
