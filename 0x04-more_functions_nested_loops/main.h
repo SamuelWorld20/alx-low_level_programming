96c58f54c13bfc0c60c2fee71e2f9b9b78d69aa0
SamuelWorld20 <ejalonibuoluwaseyi@gmail.com>
2023-06-23 16:09:17 -0700

    Solution to task 3

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void _putchar(void);
+int _putchar(char a);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+void print_most_numbers(void);
 
 #endif /* MAIN_H */
