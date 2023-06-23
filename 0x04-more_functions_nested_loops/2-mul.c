bfa71dea88f1df44383664f8e71b21c894bfa582
SamuelWorld20 <ejalonibuoluwaseyi@gmail.com>
2023-06-23 14:59:27 -0700

    Solution to task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - This function multiplies two integers.
+ * @a: First parameter that collects first integer.
+ * @b: Second parameter that collects second integer.
+ *
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	printf("%d", result);
+
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,5 +4,6 @@
 void _putchar(void);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 
 #endif /* MAIN_H */
