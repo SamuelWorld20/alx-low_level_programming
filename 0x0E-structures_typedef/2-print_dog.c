9ffffacbfdae79b01f2f85deae2a3c2a738c9f02
SamuelWorld20 <ejalonibuoluwaseyi@gmail.com>
2023-07-18 08:40:14 -0700

    Fixed error

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,8 @@ void print_dog(struct dog *d)
 {
 	if (!d)
 		return;
+
 	printf("Name: %s\n", d->name ? d->name : "(nil)");
 	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 }
