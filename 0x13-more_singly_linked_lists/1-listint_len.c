a27d59cdd8e8e75b45e1cee546538756b07e2999
SamuelWorld20 <ejalonibuoluwaseyi@gmail.com>
2023-08-01 07:42:15 -0700

    Solution to task 1 & fixed betty issue

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * listint_len - it returns the number of elements in a linked listint_t list
+ * @h: the pointer to the head of the list
+ *
+ * Return: number of elements in a linked list_t list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t = 0;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
